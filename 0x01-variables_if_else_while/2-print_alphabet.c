ac17fc6a9946f913b9cca2b7f848928cc513cc4c
chmlsud <1188@holbertonshool.com>
2019-09-19 22:58:44 +0000

    The alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - headers goes there
+ *Return: zero
+*/
+int main(void)
+{
+	char var1;
+
+	for (var1 = 'a'; var1 <= 'z'; var1++)
+		putchar(var1);
+	putchar('\n');
+	return (0);
+}
