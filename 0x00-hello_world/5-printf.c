32197404e8d130b6ac2b49e55b082461fb3d134d
chmlsud <1188@holbertonshool.com>
2019-09-19 00:40:03 +0000

    The printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Star the code
+ * printf: function is used to print
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
