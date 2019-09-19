4819f0d59724bf1db1dc232d6c194567f1ef760c
chmlsud <1188@holbertonshool.com>
2019-09-19 01:45:13 +0000

    The sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Star the code
+ * printf: function is used to print
+ * sizeof: Is the most common operator
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: %d", sizeof(char));
+	printf("Size of int: %d", sizeof(int));
+	printf("Size of long int: %d", sizeof(long int));
+	printf("Size of long long int: %d," sizeof(long long int));
+	printf("Size of float: %d,\n" sizeof(float));
+	return (0);
+}
