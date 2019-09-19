3ce36f407afa1ce29208b9bd4ae9e789127dabc0
chmlsud <1188@holbertonshool.com>
2019-09-19 00:28:25 +0000

    The puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Star the code
+ * puts: is a file handling function
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
