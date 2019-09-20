03a71043b153132c327c746275e5893cd5dee916
chmlsud <1188@holbertonshool.com>
2019-09-20 17:31:46 +0000

    Print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - This is the main function
+ *Return: Return function
+ *putchar: This is a putchar function
+*/
+int main(void)
+{
+	int var1;
+
+	for (var1 = 48; var1 < 58 ; var1++)
+	{
+		putchar(var1);
+	}
+	for (var1 = 97; var1 < 103; var1++)
+	{
+		putchar(var1);
+	}
+	putchar('\n');
+	return (0);
+}
