f5340c904b927d9dd7e0f094d6a23a3c36da05e8
chmlsud <1188@holbertonschool.com>
2019-09-24 22:15:48 +0000

    Update header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,11 +1,6 @@
-#ifndef _PUTCHAR_H_
-#define _PUTCHAR_H_
+#ifndef HLBRTN
+#define HLBRTN
 int _putchar(char c);
-#endif
-
-#ifndef _PRINT_ALPHABET_H_
-#define _PRINT_ALPHABET_H_
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
-
-
