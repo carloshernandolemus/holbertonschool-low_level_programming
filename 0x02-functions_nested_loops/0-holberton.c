b145448f85916d0660804fb648664ba5790e75ff
chmlsud <1188@holbertonschool.com>
2019-09-24 20:26:27 +0000

    The _putchar function

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Star the code
+ * _putchar: is a new function
+ *
+ * Return: 0 otherwise
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
