4c1af301cc33fd59a9f3bc14622e521d3743198b
chmlsud <1188@holbertonschool.com>
2019-09-25 14:16:04 +0000

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,15 +11,15 @@ void jack_bauer(void)
 	int var1, var2;
 
 	for (var1 = 0; var1 <= 23; var1++)
+	{
+		for (var2 = 0; var2 <= 59; var2++)
 		{
-			for (var2 = 0; var2 <= 59; var2++)
-				{
-					_putchar(var1 / 10 + '0');
-					_putchar(var1 % 10 + '0');
-					_putchar(':');
-					_putchar(var2 / 10 + '0');
-					_putchar(var2 % 10 + '0');
-					_putchar('\n');
-				}
+			_putchar(var1 / 10 + '0');
+			_putchar(var1 % 10 + '0');
+			_putchar(':');
+			_putchar(var2 / 10 + '0');
+			_putchar(var2 % 10 + '0');
+			_putchar('\n');
 		}
+	}
 }
