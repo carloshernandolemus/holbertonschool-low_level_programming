75adee41ab68b3a22360abf6fa73d6f2dbaf6359
chmlsud <1188@holbertonschool.com>
2019-09-26 19:42:04 +0000

    Multiplicate two variables

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 /**
  *mul - Is the function
  *@a: Is the first number variable function
- *@b; Is the second number variable function
+ *@b: Is the second number variable function
  *Return: Return the function
  */
 int mul(int a, int b)
