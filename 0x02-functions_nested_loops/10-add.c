59a44a9c670a03d1e2b74cd5d3207ef570be2dcc
Carlos Hernando Lemus Martínez <55101665+chmlsud@users.noreply.github.com>
2019-09-25 17:00:55 -1100

    Add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *add - This is the logical function prototype adition
+ *@var1: Variable number 1
+ *@var2: Variable number 2
+ *Return: Display return
+ */
+int add(int var1, int var2);
+{
+        int var3; 
+        var3 = var1 + var2;
+	return (var3);	
+}
