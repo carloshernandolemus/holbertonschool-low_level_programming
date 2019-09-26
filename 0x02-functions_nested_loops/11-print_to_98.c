93bc04109ca5f3fd7ff4930fc4c190802ff76b20
chmlsud <1188@holbertonschool.com>
2019-09-26 14:41:09 +0000

    The 98 number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-#include <stdio.h>
 /**
  *print_to_98 - This is the logical function prototype number
  *@var1: Variable
@@ -7,33 +5,33 @@
  */
 void print_to_98(int var1)
 {
-if (var1 < 98)
-{
-for (; var1 <= 98; var1++)
-{
-printf("%d", var1);
-if (var1 != 98)
-{
-printf(", ");
-}
-}
-printf("\n");
-}
-else if (var1 > 98)
-{
-for (; var1 >= 98; var1--)
-{
-printf("%d", var1);
-if (var1 != 98)
-{
-printf(", ");
-}
-}
-printf("\n");
-}
-else
-{
-printf("%d", var1);
-printf("\n");
-}
+	if (var1 < 98)
+	{
+		for (; var1 <= 98; var1++)
+		{
+			printf("%d", var1);
+			if (var1 != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else if (var1 > 98)
+	{
+		for (; var1 >= 98; var1--)
+		{
+			printf("%d", var1);
+			if (var1 != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("%d", var1);
+		printf("\n");
+	}
 }
