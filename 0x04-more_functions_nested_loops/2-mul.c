229e9ad5ff7f7743c6a8bca768ffe4a8fb271248
chmlsud <1188@holbertonschool.com>
2019-09-26 19:29:15 +0000

    Multiplicate two variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - Is the function
+ *@a: Is the first number variable function
+ *@b; Is the second number variable function
+ *Return: Return the function
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
