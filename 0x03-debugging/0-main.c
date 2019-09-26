9b5f4690b0ded2d9cfb818ef36aba86d23263c15
chmlsud <1188@holbertonschool.com>
2019-09-26 05:42:05 +0000

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - The function that print positive or negative integer
+ * Return: Return the finish function
+ */
+int main(void)
+{
+	int var1;
+
+	var1 = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
