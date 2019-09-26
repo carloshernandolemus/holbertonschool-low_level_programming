8e4815b7a641b4028fec1b51cfabcd938a0e1f79
Carlos Hernando Lemus Martínez <55101665+chmlsud@users.noreply.github.com>
2019-09-25 23:37:18 -0500

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,32 +6,32 @@
 void times_table(void)
 {
         int var1, var2, var3;
-
+        
         for (var1 = 0; var1 <= 9; var1++)
                 {
-                        for (var2 = 0; var2 <= 9; var2++)
+                for (var2 = 0; var2 <= 9; var2++)
+                {
+                        var3 = (var1 * var2);
+                        if (var3 <= 9)
+                        {
+                                if (var2 != 0)
                                 {
-                                        var3 = (var1 * var2);
-                                        if (var3 <= 9)
-                                                {
-                                                        if (var2 != 0)
-                                                                {
-                                                                        _putchar(' ');
-                                                                        _putchar(' ');
-                                                                }
-                                                        _putchar(var3 + '0');
-                                                }
-                                        else
-                                                {
-                                                        _putchar(' ');
-                                                        _putchar(var3 / 10 + '0');
-                                                        _putchar(var3 % 10 + '0');
-                                                }
-                                        if (var2 != 9)
-                                                {
-                                                        _putchar(',');
-                                                }
+                                        _putchar(' ');
+                                        _putchar(' ');
                                 }
-                        _putchar('\n');
+                                _putchar(var3 + '0');
+                        }
+                        else
+                        {
+                                _putchar(' ');
+                                _putchar(var3 / 10 + '0');
+                                _putchar(var3 % 10 + '0');
+                        }
+                        if (var2 != 9)
+                        {
+                                _putchar(',');
+                        }
                 }
+                _putchar('\n');
+        }
 }
