d781f472200ac1018ff3d336e87926ee766feb5f
chmlsud <1188@holbertonschool.com>
2019-09-26 17:53:57 +0000

    Is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
  *_isupper - Is the function
- *@var1: Is the variable function
+ *@c: Is the variable function
  *Return: Return the function
  */
 int _isupper(int c)
 {
 	if (c == 65)
 	{
-	return(1);
+	return (1);
 	}
 	else
 	{
-	return(0);
+	return (0);
 	}
 }
