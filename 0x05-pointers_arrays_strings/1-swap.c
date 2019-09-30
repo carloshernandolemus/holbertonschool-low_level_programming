7a8599782b822dc1c9297da1c56951d39035e213
chmlsud <1188@holbertonschool.com>
2019-09-30 21:55:02 +0000

    The swaps values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *swap_int - This is the function of change
+ *@a: This is a new value of parameter
+ *@b: This is a new value of other parameter
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
