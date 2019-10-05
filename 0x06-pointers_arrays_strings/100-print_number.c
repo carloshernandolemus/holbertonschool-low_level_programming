1829d83c1fce7465bcb71d99b03287749cde403d
chmlsud <1188@holbertonschool.com>
2019-10-05 04:36:47 +0000

    Print number

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -6,13 +6,16 @@
  */
 void print_number(int n)
 {
+	unsigned int var1 = n;
+
 	if (n < 0)
 	{
 		_putchar(45);
-		n = n * -1;
+		var1 = -var1;
 	}
-	if (n / 10)
-
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (var1 / 10)
+	{
+		print_number(var1 / 10);
+	}
+	_putchar(var1 % 10 + '0');
 }
