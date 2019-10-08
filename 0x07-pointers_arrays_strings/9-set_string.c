fe39c091dae5cbf4b3ce46b5d709108a4e858713
chmlsud <1188@holbertonschool.com>
2019-10-08 23:21:39 +0000

    Set string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include"holberton.h"
+/**
+ *set_string - Set the string of the char
+ *@s: This is a pointer of the pointer
+ *@to: This is the char
+ *Return: Return Value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
