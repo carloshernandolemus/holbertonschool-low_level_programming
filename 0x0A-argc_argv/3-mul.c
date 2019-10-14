3a8ea84d169c2e6773d968274c6336b854de228e
chmlsud <1188@holbertonschool.com>
2019-10-14 04:40:58 +0000

    Print multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - The main function of print arguments
+ *@argc: Arguments numbers
+ *@argv: This is the array
+ *Return: Return of value integer
+ */
+int main(int argc, char *argv[])
+{
+	int var1;
+	int varx;
+
+	varx = 1;
+	if (argc == 3)
+	{
+		for (var1 = 1; var1 < argc; var1++)
+		{
+			varx = varx * atoi(argv[var1]);
+		}
+		printf("%d\n", varx);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
