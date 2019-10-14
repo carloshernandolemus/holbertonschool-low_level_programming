18ec01b7491a48892b82c751ba6d4211be2a8cba
chmlsud <1188@holbertonschool.com>
2019-10-14 04:26:17 +0000

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ *@argc: Attribute unused
+ *@argv: This is the array
+ *Return: Return the value
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
