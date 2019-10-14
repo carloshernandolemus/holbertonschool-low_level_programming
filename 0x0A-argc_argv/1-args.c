3d28dcab3ab5a903d57e9d0380ea41e3f464cbf6
chmlsud <1188@holbertonschool.com>
2019-10-14 04:29:25 +0000

    First argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function of arrays
+ *@argc: Variable unused
+ *@argv: This is the array
+ *Return: Return of value integer
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", --argc);
+	return (0);
+}
