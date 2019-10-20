5c05fc1484b96bcbdffd61d0494df877c62acddd
chmlsud <1188@holbertonschool.com>
2019-10-20 22:18:37 +0000

    The returned pointer shall point to a newly allocated space in memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
-#include"holberton.h"
-#include<stdlib.h>
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  *string_nconcat - Function for concatenates
  *@s1: String location memory
@@ -9,29 +10,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int var1;
+	int var1 = 0;
 	unsigned int var2;
+	char *str;
 
-	if (s1 == '\0')
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == '\0')
+	if (s2 == NULL)
 		s2 = "";
-	for (var1 = 0; s1[var1] != '\0'; var1++)
-
-	str = malloc(sizeof(char) * (var1 + n + 1));
+	while (s1[var1] != 0)
+	{
+		var1++;
+	}
+	str = malloc((var1 + n + 1) * (sizeof(char)));
 	if (str == NULL)
+	{
 		return (NULL);
-
-	for (var2 = 0; var2 < var1; var2++)
-	{
-		str[var2] = s1[var2];
-
 	}
-	for (; var2 < (var1 + n) && s2[var2 - var1] != '\0'; var2++)
+	var1 = var2 = 0;
+	while (s1[var1] != 0)
 	{
-		str[var2] = s2[var2 - var1];
+		str[var1] = s1[var1];
+		var1++;
 	}
-	str[var2] = '\0';
+	while (var2 < n && s2[var2] != 0)
+	{
+		str[var1] = s2[var2];
+		var1++, var2++;
+	}
+	str[var1] = '\0';
 	return (str);
 }
