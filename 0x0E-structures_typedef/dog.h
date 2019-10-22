9564e4760d853879de9778a6862c5b1a67a322f2
chmlsud <1188@holbertonschool.com>
2019-10-22 17:15:08 +0000

    Update header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: Type char for the owner
  * Description: This is the structure to form data
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
