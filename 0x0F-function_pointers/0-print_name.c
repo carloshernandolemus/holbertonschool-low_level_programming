0cbc78227b532a13a27a31d56035def51bb40aa7
chmlsud <1188@holbertonschool.com>
2019-10-24 23:29:28 +0000

    Print the name from function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - Function of a pointer
+ * @name: The name pointer array
+ * @f: Function to pointer
+ * Return: Void return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	{
+		f(name);
+	}
+}
