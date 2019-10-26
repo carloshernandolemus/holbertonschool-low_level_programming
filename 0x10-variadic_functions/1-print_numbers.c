f2cd1b95fbcd1ee09efb4cfef2dbcccb1a599da8
chmlsud <1188@holbertonschool.com>
2019-10-26 19:08:43 +0000

    Fix the betty style

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,9 +2,9 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- *
- *
- *
+ * print_numbers - This is the function that prints the numbers
+ * @separator: This is the string separator
+ * @n: This is the arguments numbers
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
@@ -13,10 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(thor, n);
 
-	for(var1 = 0; var1 < n; var1++)
+	for (var1 = 0; var1 < n; var1++)
 	{
 		printf("%i", va_arg(thor, int));
-		if (var1 != (n -1) && separator != NULL)
+		if (var1 != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
