c49c091bceb3a61ed12df7ff8f72fc98524c5625
chmlsud <1188@holbertonschool.com>
2019-10-28 04:25:44 +0000

    Fix the code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,35 +9,26 @@
 void print_all(const char * const format, ...)
 {
 	va_list thor;
-	unsigned int var1 = 0, var2;
+	unsigned int var1 = 0, var2, var3 = 0;
 	char *str;
 	const char arg[] = "cifs";
 
 	va_start(thor, format);
 	while (format && format[var1])
 	{
-		var2 = 0;
-		while (arg[var2])
-		{
-			if (format[var1] == arg[var2])
-			{
-				printf(", ");
-				break;
-			} var2++;
-		}
 		switch (format[var1])
 		{
 		case 'c':
-			printf("%c", va_arg(thor, int));
+			printf("%c", va_arg(thor, int)), var3 = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(thor, int));
+			printf("%d", va_arg(thor, int)), var3 = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(thor, double));
+			printf("%f", va_arg(thor, double)), var3 = 1;
 			break;
 		case 's':
-			str = va_arg(thor, char *);
+			str = va_arg(thor, char *), var3 = 1;
 			if (!str)
 			{
 				printf("(nil)");
@@ -45,7 +36,17 @@ void print_all(const char * const format, ...)
 			}
 			printf("%s", str);
 			break;
-		} var1++;
+		}
+		var1++;
+		var2 = 0;
+		while (arg[var2])
+		{
+			if (format[var1] == arg[var2] && var3)
+			{
+				printf(", ");
+				break;
+			}var2++;
+		}
 	}
 	va_end(thor), putchar('\n');
 }
