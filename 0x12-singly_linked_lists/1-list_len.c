3cc56aa131dc37fc810718c3a93309940a004aae
chmlsud <1188@holbertonschool.com>
2019-10-31 17:16:53 +0000

    Prints numbers of the elements in the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * list_len - Prints numbers of elements in the list
+ * @h: List
+ * Return: Number of elements in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t thor;
+	thor = 0;
+	while (h != NULL)
+	{
+		/*if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			}*/
+		h = h->next;
+		thor++;
+	}
+	return (thor);
+}
