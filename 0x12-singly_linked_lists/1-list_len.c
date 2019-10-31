8b7ed26d8ec1f10bade06eb1f5a46cf569296505
chmlsud <1188@holbertonschool.com>
2019-10-31 17:27:17 +0000

    Fix the betty style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -15,14 +15,6 @@ size_t list_len(const list_t *h)
 	thor = 0;
 	while (h != NULL)
 	{
-		/*if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-			}*/
 		h = h->next;
 		thor++;
 	}
