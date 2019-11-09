bea65b3402310b598617cb4967cced75b86e65ae
chmlsud <1188@holbertonschool.com>
2019-11-09 16:07:39 +0000

    Fix the betty style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,10 +3,9 @@
 #include <math.h>
 #include "holberton.h"
 /**
- * get_bit - returns the value of a bit at a given index.
- * @n: number
- * @index: index
- * Description: prints the binary representation of a number
+ * get_bit - Returns the value of a bit
+ * @n: Unsigned number
+ * @index: Unsigned index
  * Return: value of a bit
  */
 int get_bit(unsigned long int n, unsigned int index)
