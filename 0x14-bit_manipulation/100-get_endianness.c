ba703797d823f87b3bda02d0122275d7d83aeee5
chmlsud <1188@holbertonschool.com>
2019-11-09 16:30:24 +0000

    Function that get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "holberton.h"
+/**
+ *get_endianness - Checks the endianness
+ *Return: Return the result
+ */
+int get_endianness(void)
+{
+	unsigned int var1;
+	char *str = (char *)&var1;
+
+	var1 = 1;
+	if (*str)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
