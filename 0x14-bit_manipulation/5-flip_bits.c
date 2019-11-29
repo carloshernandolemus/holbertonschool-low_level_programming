1009deacf72793f49513d127f9a910f43d97e032
chmlsud <1188@holbertonschool.com>
2019-11-29 16:41:40 +0000

    Fix the variables

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,17 +8,18 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int var1;
+	unsigned long int tem;
 	int i = 0;
 
-	var1 = n ^ m;
-	while (var1)
+	tem = n ^ m;
+	while (tem)
 	{
-		if (var1 & 1)
-			var2++;
+		if (tem & 1)
+			i++;
 
-		var1 = var1 >> 1;
+		tem = tem >> 1;
 	}
 
-	return (var2);
+	return (i);
+
 }
