209239d89f6aec0df64c18799b9eea5019aaa95c
chmlsud <1188@holbertonschool.com>
2019-11-29 16:40:26 +0000

    Fix the variables

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
 #include "holberton.h"
 /**
  *flip_bits - Number of bits to flip
- *@n:unsigned start number
- *@m:unsigned final number
- *Return: 1 (true) -1 (false)
+ *@n: Unsigned long int start number
+ *@m: Unsigned long int final number,
+ *Return: 1 if it works or -1 if it does not
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int var1;
-	int var2 = 0;
+	int i = 0;
 
 	var1 = n ^ m;
 	while (var1)
@@ -21,5 +19,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 		var1 = var1 >> 1;
 	}
+
 	return (var2);
 }
