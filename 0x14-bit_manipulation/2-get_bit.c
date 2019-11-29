ea5bf8ef99ef078a9bcf0abcc846ecbe412e2075
chmlsud <1188@holbertonschool.com>
2019-11-29 16:32:28 +0000

    Fix the function

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
 #include "holberton.h"
 /**
- * get_bit - Returns the value of a bit
- * @n: Unsigned number
- * @index: Unsigned index
- * Return: value of a bit
+ *get_bit - Get the bit
+ *@n: unsigned int to be evaluated
+ *@index: Index to check
+ *Return: bit in the index or -1 if fails
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 64)
-		return (-1);
+	unsigned int var1 = 0;
 
-	if (((n >> index) & 1) == 1)
+	while (n)
 	{
-		return (1);
+		if (var1 == index)
+			return (n % 2);
+
+		n = n / 2;
+		var1++;
 	}
-	else
-	{
+	if (index > var1 && index < 63)
 		return (0);
-	}
+
+	return (-1);
 }
