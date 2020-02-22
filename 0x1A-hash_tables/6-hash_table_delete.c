e44f9a10dc1fe68fe0d8783f94735fd0e4143e1e
chmlsud <1188@holbertonschool.com>
2020-02-22 03:03:56 +0000

    Fix the output code

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,48 +2,32 @@
 #include "hash_tables.h"
 
 /**
- * free_content_at_index - Free linked list at given index
- * @ht: Hash table
- * @index: Position in the array to free
- * Return: 1 on success
- * On error: 0 on fail
- */
-
-int free_content_at_index(hash_table_t *ht, unsigned long index)
-{
-	hash_node_t *current, *after;
-
-	if (!ht || index >= ht->size)
-		return (0);
-
-	current = ht->array[index];
-	for (; current; current = after)
-	{
-		after = current->next;
-		free(current->value), free(current->key);
-		free(current);
-	}
-
-	return (1);
-}
-
-/**
- * hash_table_delete - Function that deletes a hash table.
- * @ht: Hash table
- * Return: nothing
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash table to delete.
+ *
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long i = 0;
+	unsigned long int idx = 0;
+	hash_node_t *pos, *aux;
 
 	if (!ht)
 		return;
 
-	for (; i < ht->size; i++)
-		if (ht->array[i])
-			free_content_at_index(ht, i);
-
+	for ( ; idx < ht->size; idx++)
+	{
+		pos = ht->array[idx];
+		while (pos)
+		{
+			aux = pos->next;
+			free(pos->key);
+			free(pos->value);
+			free(pos);
+			pos = aux;
+		}
+	}
 	free(ht->array);
 	free(ht);
+
 }
